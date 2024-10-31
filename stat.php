3743a1a53881715e5e75ae76c44dcf15e680939e
myownfreehost-templates <137825550+myownfreehost-templates@users.noreply.github.com>
2024-10-31 11:31:34 +0400

    Add files via upload

---
+++ stat.php
@@ -0,0 +1,4 @@
+                <!-- TOP.GE ASYNC COUNTER CODE -->
+                <div id="top-ge-counter-container" data-site-id="117433"></div>
+                <script async src="//counter.top.ge/counter.js"></script>
+                <!-- / END OF TOP.GE COUNTER CODE -->
