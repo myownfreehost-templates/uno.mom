70b62726e55ca921e5345537eeef6e7e89df85ec
myownfreehost-templates <137825550+myownfreehost-templates@users.noreply.github.com>
2024-11-24 13:02:17 +0400

    Delete stat.php

--- stat.php
+++
@@ -1,5 +0,0 @@
-            <!-- TOP.GE ASYNC COUNTER CODE -->
-            <div id="top-ge-counter-container" data-site-id="117686"></div>
-            <script async src="//counter.top.ge/counter.js"></script>
-            <!-- / END OF TOP.GE COUNTER CODE -->
-        
